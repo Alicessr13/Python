c7524a88acea5c2a8a0dcd21716ffc551f284137
Alice Stephanie Soares Ribeiro <alicessr12@gmail.com>
2023-10-30 23:42:09 -0300

    aula 1
    curso em video

--- Primeiros_passos/teste.py
+++ Primeiros_passos/teste.py
@@ -1,1 +1,15 @@
 print('Estou aprendendo Python!')
+
+print('Olá, Mundo!') # (Olá, Mundo!) gera erro
+
+print(7+4)
+
+print('7'+'4')
+
+print('Ola' , 5) #('Ola' + 5) gera erro
+
+nome = 'Alice'
+idade = 20
+peso = 49.56
+
+print(nome, idade, peso) #print(nome + idade + peso) gera erro
